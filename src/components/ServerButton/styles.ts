c197460ca2e9778f37f100b88d35e623fa5fceb9
luis-antonio-ac <luis.developer.ac@gmail.com>
2020-08-22 18:50:26 -0500

    Server List

---
+++ src/components/ServerButton/styles.ts
@@ -0,0 +1,74 @@
+import styled from 'styled-components';
+
+import { Props } from '.';
+
+export const Button = styled.button<Props>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-shrink: 0;
+
+  width: 48px;
+  height: 48px;
+  border-radius: 50%;
+
+  margin-bottom: 8px;
+
+  background-color: ${(props) =>
+    props.isHome ? 'var(--rocketseat)' : 'var(--primary)'};
+
+  cursor: pointer;
+  position: relative;
+  transition: all 0.2s;
+
+  > img {
+    width: 100%;
+    height: 100%;
+    border-radius: 50%;
+  }
+
+  &::before {
+    width: 9px;
+    height: 9px;
+
+    position: absolute;
+    left: -10px;
+    top: calc(50% - 4.5px);
+
+    background-color: var(--white);
+    border-radius: 50%;
+
+    content: '';
+    display: ${props => props.hasNotifications ? 'inline' : 'none'};
+  }
+
+  &::after {
+    background-color: var(--notification);
+    width: auto;
+    height: 16px;
+
+    padding: 0 4px;
+
+    position: absolute;
+    bottom: -4px;
+    right: -4px;
+
+    border-radius: 12px;
+    border: 4px solid var(--quaternary);
+
+    text-align: right;
+    font-size: 13px;
+    font-weight: bold;
+    color: #fff;
+
+    content: '${props => props.mentions && props.mentions}';
+    display: ${props => props.mentions && props.mentions > 0 ? 'inline' : 'none'};
+  }
+
+  &.active,
+  &:hover {
+    border-radius: 16px;
+    background-color: ${(props) =>
+      props.isHome ? 'var(--rocketseat)' : 'var(--discord)'};
+  }
+`;
---
+++ src/components/ServerList/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import { Container } from './styles';
+import ServerButton from '../ServerButton';
+
+const ServerList: React.FC = () => {
+  return (
+    <Container>
+      <ServerButton isHome={true} />
+
+      {/* <Separator /> */}
+
+      <ServerButton />
+      <ServerButton />
+      <ServerButton hasNotifications={true} />
+      <ServerButton />
+      <ServerButton />
+      <ServerButton />
+      <ServerButton mentions={4} />
+      <ServerButton />
+      <ServerButton />
+      <ServerButton />
+      <ServerButton />
+      <ServerButton />
+      <ServerButton />
+    </Container>
+  );
+}
+
+export default ServerList;
