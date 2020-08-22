2cf73140a2f8626a12bfbb8476214c2aa6f1e48f
luis-antonio-ac <luis.developer.ac@gmail.com>
2020-08-22 17:50:45 -0500

    Global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React from 'react';
+
+import GlobalStyles from './styles/GlobalStyles';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <GlobalStyles />
+      <div className="App"></div>
+    </>
+  );
 }
 
 export default App;
