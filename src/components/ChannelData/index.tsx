deb6c447d23d66cf57237e40af428669a162119b
luis-antonio-ac <luis.developer.ac@gmail.com>
2020-08-23 18:20:17 -0500

    Scroll messages

--- src/components/ChannelData/index.tsx
+++ src/components/ChannelData/index.tsx
@@ -1,104 +1,113 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 
 import { Container, Messages, InputWrapper, Input, InputIcon } from './styles';
-import ChannelMessage, { Mention } from '../ChannelMessage'
-
+import ChannelMessage, { Mention } from '../ChannelMessage';
 
 const ChannelData: React.FC = () => {
+  const messagesRef = useRef() as React.MutableRefObject<HTMLDivElement>;
+
+  useEffect(() => {
+    const div = messagesRef.current;
+
+    if(div) {
+      div.scrollTop = div.scrollHeight;
+    }
+  }, [messagesRef]);
+
   return (
     <Container>
-      <Messages>
-        <ChannelMessage 
+      <Messages ref={messagesRef}>
+        <ChannelMessage
           author="Luís Antônio"
           date="05/02/2020"
           content="Hoje é meu aniversário"
         />
-        <ChannelMessage 
+        <ChannelMessage
           author="Luís Antônio"
           date="05/02/2020"
           content="Hoje é meu aniversário"
         />
-        <ChannelMessage 
+        <ChannelMessage
           author="Luís Antônio"
           date="05/02/2020"
           content="Hoje é meu aniversário"
         />
-<ChannelMessage 
+        <ChannelMessage
           author="Luís Antônio"
           date="05/02/2020"
           content="Hoje é meu aniversário"
         />
-<ChannelMessage 
+        <ChannelMessage
           author="Luís Antônio"
           date="05/02/2020"
           content="Hoje é meu aniversário"
         />
-<ChannelMessage 
+        <ChannelMessage
           author="Luís Antônio"
           date="05/02/2020"
           content="Hoje é meu aniversário"
         />
-<ChannelMessage 
+        <ChannelMessage
           author="Luís Antônio"
           date="05/02/2020"
           content="Hoje é meu aniversário"
         />
-<ChannelMessage 
+        <ChannelMessage
           author="Luís Antônio"
           date="05/02/2020"
           content="Hoje é meu aniversário"
         />
-<ChannelMessage 
+        <ChannelMessage
           author="Luís Antônio"
           date="05/02/2020"
           content="Hoje é meu aniversário"
         />
-<ChannelMessage 
+        <ChannelMessage
           author="Luís Antônio"
           date="05/02/2020"
           content="Hoje é meu aniversário"
         />
-<ChannelMessage 
+        <ChannelMessage
           author="Luís Antônio"
           date="05/02/2020"
           content="Hoje é meu aniversário"
         />
-<ChannelMessage 
+        <ChannelMessage
           author="Luís Antônio"
           date="05/02/2020"
           content="Hoje é meu aniversário"
         />
-<ChannelMessage 
+        <ChannelMessage
           author="Luís Antônio"
           date="05/02/2020"
           content="Hoje é meu aniversário"
         />
-<ChannelMessage 
+        <ChannelMessage
           author="Luís Antônio"
           date="05/02/2020"
           content="Hoje é meu aniversário"
         />
-<ChannelMessage 
+        <ChannelMessage
           author="Luís Antônio"
           date="05/02/2020"
           content="Hoje é meu aniversário"
         />
-<ChannelMessage 
+        <ChannelMessage
           author="Luís Antônio"
           date="05/02/2020"
           content="Hoje é meu aniversário"
         />
-<ChannelMessage 
+        <ChannelMessage
           author="Luís Antônio"
           date="05/02/2020"
           content="Hoje é meu aniversário"
         />
-<ChannelMessage 
+        <ChannelMessage
           author="Luís Antônio"
           date="05/02/2020"
           content="Hoje é meu aniversário"
         />
-        <ChannelMessage 
+        <ChannelMessage
           author="Diego Fernandez"
           date="05/02/2020"
           isBot
@@ -109,45 +118,41 @@ const ChannelData: React.FC = () => {
             </>
           }
         />
-<ChannelMessage 
+        <ChannelMessage
           author="Luís Antônio"
           date="05/02/2020"
           content="Hoje é meu aniversário"
         />
-<ChannelMessage 
+        <ChannelMessage
           author="Luís Antônio"
           date="05/02/2020"
           content="Hoje é meu aniversário"
         />
-<ChannelMessage 
+        <ChannelMessage
           author="Luís Antônio"
           date="05/02/2020"
           content="Hoje é meu aniversário"
         />
-<ChannelMessage 
+        <ChannelMessage
           author="Luís Antônio"
           date="05/02/2020"
           content="Hoje é meu aniversário"
         />
-<ChannelMessage 
+        <ChannelMessage
           author="Luís Antônio"
           date="05/02/2020"
           content="Hoje é meu aniversário"
         />
-<ChannelMessage 
+        <ChannelMessage
           author="Luís Antônio"
           date="05/02/2020"
           content="Hoje é meu aniversário"
         />
-<ChannelMessage 
+        <ChannelMessage
           author="Luís Antônio"
           date="05/02/2020"
           content="Hoje é meu aniversário"
         />
-
-        
-
-        
       </Messages>
 
       <InputWrapper>
